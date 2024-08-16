9d6808a816e8f3094d4cb5e9a7fc20a00823d6a5
Giannis Kosmas <kosmasgiannis@gmail.com>
2024-08-16 16:13:29 +0300

    Adjustments and improvements

--- bin/whatismyip.js
+++ bin/whatismyip.js
@@ -1,35 +1,44 @@
 #!/usr/bin/env node
 
-var program = require('commander');
-var util = require('util');
-var ip = require('../lib/whatismyip.js');
+const { program } = require('commander');
+const util = require('util');
+const ip = require('../lib/whatismyip.js');
+const _package = require('../package.json');
 
 function list(val) {
   return val.split(',');
 }
 
 program
-  .version('0.0.1')
+  .version(_package.version)
   .option('-a, --all', 'Hit all servers')
   .option('-u, --urls <list>', 'A comma separated list of urls of servers to lookup', list)
   .option('-6, --ipv6', 'Check for ipv6 address')
   .option('-v, --verbose', 'Be verbose')
   .parse(process.argv);
 
+const opts = program.opts();
+
 var hosts = [];
 
-if (program.urls) {
-  program.urls.forEach(function(u) {
+if (opts.urls) {
+  opts.urls.forEach(function(u) {
     hosts.push({'url' : u, 'truncate' : '', 'matchIndex' : 0});
   });
 } else {
-  if (program.ipv6) {
+  if (opts.ipv6) {
     hosts = [
       {url:'http://ipv6.whatismyv6.com/', truncate:''},
-      {url:'https://v6.ifconfig.co/ip', truncate:''}
+      {url:'https://v6.ifconfig.co/ip', truncate:''},
+      {url:'https://icanhazip.com', truncate:''},
+      {url:'https://ident.me', truncate:''},
+      {url:'https://curlmyip.net', truncate:''}
     ];
   } else {
     hosts = [
+      {url:'https://ipinfo.io/ip/', truncate:''},
+      {url:'https://ipecho.net/plain', truncate:''},
+      {url:'https://ipaddr.site', truncate:''},
       {url:'https://api.ipify.org/', truncate:''},
       {url:'https://ip.seeip.org/', truncate:''},
       {url:'https://ifconfig.me/ip', truncate:''},
@@ -47,18 +56,20 @@ if (program.urls) {
 
 var start = Date.now();
 
+var host = '';
+var options = {};
+const func = (opts.ipv6 === true) ? ip.whatismyipv6 : ip.whatismyipv4;
 for (var hostId in hosts) {
-  var host = hosts[hostId]
-  var options = {
-        url: host['url'],
-        truncate: host['truncate'],
-        matchIndex: host['matchIndex']
-      };
+  host = hosts[hostId]
+  options = {
+    url: host['url'],
+    truncate: host['truncate'],
+    matchIndex: host['matchIndex']
+  };
 
-  var func = (program.ipv6 === true) ? ip.whatismyipv6 : ip.whatismyipv4;
   func(options, function(err, data){
     if (!err) {
-      if (program.verbose) {
+      if (opts.verbose) {
         console.log('From '+data.url+' : '+((data.ip) ? data.ip : 'not resolved,')+' time taken : ', data.time - start);
       } else {
         if (data.ip) {
@@ -67,11 +78,11 @@ for (var hostId in hosts) {
           console.warn('Could not resolve ip from the specified host(s).')
         }
       }
-      if ((! program.all) || (!program.all && ! program.verbose)) {
+      if ((! opts.all) || (!opts.all && ! opts.verbose)) {
         process.exit();
       }
     } else {
-      if (program.verbose) {
+      if (opts.verbose) {
         console.log('From '+data.url+' : '+((data.ip) ? data.ip : 'not resolved,')+' time taken : ', data.time - start, 'Error:', util.inspect(data.error,{depth:null}));
       }
     }
